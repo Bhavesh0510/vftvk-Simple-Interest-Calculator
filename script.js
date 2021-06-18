9a8a422a20f1a7006b1b6208db24122ac9c7cd9c
Bhavesh0510 <65763672+Bhavesh0510@users.noreply.github.com>
2021-06-18 15:38:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
     var year = new Date().getFullYear()+parseInt(years);
     var interest = principal * years * rate /100;
 	
- document.getElementById("result").innerHTML="</br>If you deposit <mark>" + principal + "</mark>,</br> at an interest rate of <mark>" + rate + "%." +"</mark></br> You will receive an amount of <mark>"  + interest + "</mark>,</br> in the year <mark>" + year + "</mark>";
+ document.getElementById("result").innerHTML="</br>If you deposit <mark>" + principal + "</mark>,</br> at an interest rate of <mark>" + rate + "%" +"</mark>.</br> You will receive an amount of <mark>"  + interest + "</mark>,</br> in the year <mark>" + year + "</mark>";
 }
 
     function updateRate(newVal) 
