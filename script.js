e6537d2ec768cb90dba298607b1fe8a9104b4401
Bhavesh0510 <65763672+Bhavesh0510@users.noreply.github.com>
2021-06-18 14:04:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     var year = new Date().getFullYear()+parseInt(years);
     var interest = principal * years * rate /100;
 	
- document.getElementById("result").innerHTML="If you deposit "+<span style="backgrond-color:yellow;">principal</span>+",\<br\>at an interest rate of "+<span style="backgrond-color:yellow;">rate</span>+"%\<br\>You will receive an amount of "+<span style="backgrond-color:yellow;">interest</span>+",\<br\>in the year "+<span style="backgrond-color:yellow;">year</span>+"\<br\>"
+ document.getElementById("result").innerHTML="If you deposit "+<span style="backgrond-color:yellow;">principal</span>+",\<br\>at an interest rate of "+<span style="backgrond-color:yellow;">rate</span>+"%.\<br\>You will receive an amount of "+<span style="backgrond-color:yellow;">interest</span>+",\<br\>in the year "+<span style="backgrond-color:yellow;">year</span>+"\<br\>"
 }
 
     function updateRate(newVal) 
