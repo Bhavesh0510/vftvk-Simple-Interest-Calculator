f1457b2e992dc6adabab1f1cc0b8e83ab5f960d0
Bhavesh0510 <65763672+Bhavesh0510@users.noreply.github.com>
2021-06-18 12:38:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
     function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 
 }
  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
